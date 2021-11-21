f0aae9eaff4a6c283ac2831842b8b7adf0fd0f90
4souzza <arthurfarias93@gmail.com>
2021-11-21 15:41:34 -0300

    @4souzza [task: CREATED UsuarioRepositoryTest]

--- mangueTech/src/main/java/com/manguetech/seguranca/UserDetailServiceImpl.java
+++ mangueTech/src/main/java/com/manguetech/seguranca/UserDetailServiceImpl.java
@@ -3,6 +3,7 @@ package com.manguetech.seguranca;
 import java.util.Optional;
 
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
